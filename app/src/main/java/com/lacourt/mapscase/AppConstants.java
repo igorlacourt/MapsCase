cc26f8c57d3e9688a5217921c5262bb03d95033c
globalsys.igor <globalsys.igor@meupag.com.br>
2020-01-13 18:08:06 -0300

    Comment modification.

--- app/src/main/java/com/lacourt/mapscase/AppConstants.java
+++ app/src/main/java/com/lacourt/mapscase/AppConstants.java
@@ -1,6 +1,6 @@
 package com.lacourt.mapscase;
 
 public class AppConstants {
-//    http://api.openweathermap.org/data/2.5/find?lat=-20.3019027&lon=-40.2850312&cnt=15&APPID=c4f55aab573b533814bc16c65db7a110
+//    http://api.openweathermap.org/data/2.5/find?lat=-20.3019027&lon=-40.2850312&cnt=15&APPID=apikey
     public static String WEATHER_BASE_URL = "http://api.openweathermap.org/";
 }
