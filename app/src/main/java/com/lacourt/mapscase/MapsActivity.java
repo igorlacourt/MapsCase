aa8235f3e9a45190c1be94ec0884d752624e3ecd
globalsys.igor <globalsys.igor@meupag.com.br>
2020-01-13 15:38:30 -0300

    Marker displaying when clicking implemented.

--- app/src/main/java/com/lacourt/mapscase/MapsActivity.java
+++ app/src/main/java/com/lacourt/mapscase/MapsActivity.java
@@ -1,28 +1,31 @@
 package com.lacourt.mapscase;
 
-import androidx.fragment.app.FragmentActivity;
-
-import android.location.Address;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int LOCATION_REQUEST_CODE = 1;
     private GoogleMap mMap;
     private LatLng latLng;
     private Marker marker;
@@ -33,114 +36,97 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-//                .findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
-        geocoder = new Geocoder(this, Locale.getDefault());
-
-        setUpMapIfNeeded();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+//        geocoder = new Geocoder(this, Locale.getDefault());
     }
+
     @Override
     protected void onResume() {
         super.onResume();
-        setUpMapIfNeeded();
+//        setUpMapIfNeeded();
     }
+
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                    .findFragmentById(R.id.map);
 
-            if(mapFragment != null)
+            if (mapFragment != null)
                 mapFragment.getMapAsync(this);
 
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
+
         }
     }
-    private void setUpMap() {
 
+    private void setUpMap() {
         mMap.setMyLocationEnabled(true);
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         mMap.getUiSettings().setMapToolbarEnabled(false);
 
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        @SuppressLint("MissingPermission")
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        final LatLng currentLocation = new LatLng(longitude, latitude);
+
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(currentLocation)
+                .title("Local Escolhido");
+
+        mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+
+
+//         Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void onMapClick() {
+        // Setting a click event handler for the map
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 
             @Override
-            public void onMapClick(LatLng point) {
-                //save current location
-                latLng = point;
+            public void onMapClick(LatLng latLng) {
 
-                List<Address> addresses = new ArrayList<>();
-                try {
-                    addresses = geocoder.getFromLocation(point.latitude, point.longitude,1);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
 
-                android.location.Address address = addresses.get(0);
+                // Setting the position for the marker
+                markerOptions.position(latLng);
 
-                if (address != null) {
-                    StringBuilder sb = new StringBuilder();
-                    for (int i = 0; i < address.getMaxAddressLineIndex(); i++){
-                        sb.append(address.getAddressLine(i) + "\n");
-                    }
-                    Toast.makeText(MapsActivity.this, sb.toString(), Toast.LENGTH_LONG).show();
-                }
+                // Setting the title for the marker.
+                // This will be displayed on taping the marker
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
 
-                //remove previously placed Marker
-                if (marker != null) {
-                    marker.remove();
-                }
+                // Clears the previously touched position
+                mMap.clear();
 
-                //place marker where user just clicked
-                marker = mMap.addMarker(new MarkerOptions().position(point).title("Marker")
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+                // Animating to the touched position
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
 
-            }
-        });
-
-    }
-    private void onMapClick(){
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-
-            @Override
-            public void onMapClick(LatLng point) {
-                //save current location
-                latLng = point;
-
-                List<Address> addresses = new ArrayList<>();
-                try {
-                    addresses = geocoder.getFromLocation(point.latitude, point.longitude,1);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                android.location.Address address = addresses.get(0);
-
-                if (address != null) {
-                    StringBuilder sb = new StringBuilder();
-                    for (int i = 0; i < address.getMaxAddressLineIndex(); i++){
-                        sb.append(address.getAddressLine(i) + "\n");
-                    }
-                    Toast.makeText(MapsActivity.this, sb.toString(), Toast.LENGTH_LONG).show();
-                }
-
-                //remove previously placed Marker
-                if (marker != null) {
-                    marker.remove();
-                }
-
-                //place marker where user just clicked
-                marker = mMap.addMarker(new MarkerOptions().position(point).title("Marker")
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
-
+                // Placing a marker on the touched position
+                mMap.addMarker(markerOptions);
             }
         });
     }
+
+    private void requestPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            setUpMap();
+            onMapClick();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+        }
+    }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -153,10 +139,75 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        requestPermission();
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (permissions.length == 1 &&
+                    permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setUpMap();
+                onMapClick();
+            } else {
+                noPermissionDialog();
+            }
+        }
+    }
+
+    private void noPermissionDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Sem permissão")
+                .setMessage("Você precisa conceder acesso à sua localização para que o app funcione.")
+
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        requestPermission();
+                    }
+                })
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
+
+//        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+//
+//            @Override
+//            public void onMapClick(LatLng point) {
+//                //save current location
+//                latLng = point;
+//
+//                List<Address> addresses = new ArrayList<>();
+//                try {
+//                    addresses = geocoder.getFromLocation(point.latitude, point.longitude,1);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                android.location.Address address = addresses.get(0);
+//
+//                if (address != null) {
+//                    StringBuilder sb = new StringBuilder();
+//                    for (int i = 0; i < address.getMaxAddressLineIndex(); i++){
+//                        sb.append(address.getAddressLine(i) + "\n");
+//                    }
+//                    Toast.makeText(MapsActivity.this, sb.toString(), Toast.LENGTH_LONG).show();
+//                }
+//
+//                //remove previously placed Marker
+//                if (marker != null) {
+//                    marker.remove();
+//                }
+//
+//                //place marker where user just clicked
+//                marker = mMap.addMarker(new MarkerOptions().position(point).title("Marker")
+//                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+//
+//            }
+//        });
