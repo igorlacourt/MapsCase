b7614aa4088a56dc87236d3c41e8ade15d2d9bc0
igor lacourt <igorlacourt@gmail.com>
2020-01-17 06:26:46 -0300

    privacy policy text inserted in AppConstants.

--- app/src/main/java/com/lacourt/mapscase/AppConstants.java
+++ app/src/main/java/com/lacourt/mapscase/AppConstants.java
@@ -2,5 +2,6 @@ package com.lacourt.mapscase;
 
 public class AppConstants {
 //    http://api.openweathermap.org/data/2.5/find?lat=-20.3019027&lon=-40.2850312&cnt=15&APPID=apikey
-    public static String WEATHER_BASE_URL = "https://api.openweathermap.org/";
+    public static final String WEATHER_BASE_URL = "https://api.openweathermap.org/";
+    public static final String PRIVICY_POLICY = "O aplicativo \"MapsCase\" contém a tecnologia de geolocalização de propriedade da IN LOCO TECNOLOGIA DA INFORMAÇÃO S.A. (a “In Loco”). Essa tecnologia permite a captura dos dados de localização e recorrência de uso do aplicativo, que são coletados e tratados, de forma anônima, pela In Loco, de maneira que você possa receber Notificações Push personalizadas no aplicativo. Ao aceitar estes Termos de Uso, o usuário declara que está ciente e que concorda com a coleta e tratamento de seus dados pela In Loco, tal como disposto na Política de Privacidade da In Loco."
 }
